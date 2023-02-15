d4ca01e8c5217723caa60fe96a123cb7da892c17
fwespi <fabian_wespi@hispeed.ch>
2023-02-15 16:22:11 +0100

    Add files via upload

--- src/utilities/main.js
+++ src/utilities/main.js
@@ -3,22 +3,19 @@
 })();
 
 function substituteCharacters (event, mapping) {
-	Word.run( async context => {		
+	Word.run( async context => {
 		const selection = context.document.getSelection();
+		context.load(selection, "text");
+		await context.sync();
+		
+		let txt = selection.text;
 		for(let n=0; n<mapping.length; n++){
-			const char = mapping[n][0];
-			const results = selection.search(char, {matchCase: true });
-			context.load(results);
-			await context.sync();
-			for(let i = 0; i < results.items.length; i++) {
-				results.items[i].insertText(mapping[n][1], "replace");
-			};
-		};
+			txt.replaceAll(mapping[n][0], mapping[n][1])
+		}
+		selection.insertText(txt, "replace");
+		
 		await context.sync();
 	});
-	
-	if(event)
-		event.completed();
 };
 
 async function convertTransliteration (event) {
