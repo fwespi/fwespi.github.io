1001325f2f19fb698ed106e5ae727e22677d5821
fwespi <fabian_wespi@hispeed.ch>
2023-10-25 06:54:09 +0200

    Add files via upload

--- src/utilities/main.js
+++ src/utilities/main.js
@@ -1,5 +1,24 @@
 let wasm;
 
+const registerServiceWorker = async () => {
+	if("serviceWorker" in navigator){
+		try {
+			const registration = await navigator.serviceWorker.register("./utilities/sw.js", {scope: "../"});
+			if(registration.installing){
+				console.log("Service worker installing");
+			} else if(registration.waiting){
+				console.log("Service worker installed");
+			} else if(registration.active){
+				console.log("Service worker active");
+			}
+		} catch (error) {
+			console.error(`Registration failed with ${error}`);
+		}
+	}
+}
+
+registerServiceWorker();
+
 (() => {
 	Office.initialize =  (reason) => {};
 	
